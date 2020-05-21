8d4f69ceb5d3f8d15bfa8140a52a19ed652a0049
Corentin <corentin.colart@gmail.com>
2020-05-21 19:13:39 +0200

    Test phase day two, update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,23 +33,33 @@ function preload() { //preload any existing assets with two arguments : name et 
         frameWidth: 32,
         frameHeight: 32
     });
+
+    this.load.audio('gold_pickup', ['assets/audio/Pickup.wav']); //selects an array of audio to be played (if browser does not support)
 };
 
 function create() { //adds the preloaded asset to the scene
+    let goldPickupSound = this.sound.add('gold_pickup'); //can add a second argument with different object properties 
+
     let button = this.add.image(100, 100, 'button1'); // arguments : coordinates (0,0 is top left)
 
     button.setOrigin(0.5, 0.5);
 
     this.add.sprite(300, 100, 'button1'); //sprite can be animated, image not
 
-    this.add.image(300, 300, 'items', 4); //fourth argument select the frame (starts at 0)
+    this.chest = this.physics.add.image(300, 300, 'items', 0); //fourth argument select the frame (starts at 0)
 
-    this.physics.add.image(500, 100, 'button1'); //creates new object with applied physic
+    this.wall = this.physics.add.image(500, 100, 'button1'); //creates new object with applied physic
+    this.wall.setImmovable(); //won't make the object move even with applied physics 
 
     this.player = this.physics.add.image(32, 32, 'characters', 0);
     this.player.setScale(2); //sets y and x values if not specified sets both
+    this.player.body.setCollideWorldBounds(true); //makes the pklayer unable to exit the scene
+
+    this.physics.add.collider(this.player, this.wall); //Adds collision between said objects
+    this.physics.add.overlap(this.player, this.chest, function(player, chest){goldPickupSound.play(); chest.destroy();}); //adds overlap check, plays a sound and destroys it
 
     this.cursors = this.input.keyboard.createCursorKeys(); //Phaser in-built keyboard event listener
+
 };
 
 function update() { //updates keyboard event listener
