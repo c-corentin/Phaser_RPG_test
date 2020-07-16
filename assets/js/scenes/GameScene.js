36d86934fd8923f6ac3ac36d325be2e544b02afa
Corentin <corentin.colart@gmail.com>
2020-07-16 12:21:19 +0200

    Map.js class created

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -80,27 +80,4 @@ class GameScene extends Phaser.Scene {
     // spawn a new chest
     this.time.delayedCall(1000, this.spawnChest, [], this);
   }
-
-  createMap() {
-    // create the tile map
-    this.map = this.make.tilemap({ key: 'map' });
-    // add the tileset image to our map
-    this.tiles = this.map.addTilesetImage('background', 'background', 32, 32, 1, 2);
-    // create our background
-    this.backgroundLayer = this.map.createStaticLayer('background', this.tiles, 0, 0);
-    // create blocked layer
-    this.blockedLayer = this.map.createStaticLayer('blocked', this.tiles, 0, 0);
-    this.blockedLayer.setCollisionByExclusion([-1]);
-
-    // set scale to match the player's 
-    this.backgroundLayer.setScale(2);
-    this.blockedLayer.setScale(2);
-
-    // set world bounds on map instead of view
-    this.physics.world.bounds.width = this.map.widthInPixles * 2;
-    this.physics.world.bounds.height = this.map.heightInPixles * 2;
-
-    // set the camera inside the map
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixles * 2, this.map.heightInPixles * 2);
-  }
 }
