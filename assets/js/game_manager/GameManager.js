3ecd9e0c27632cfcb5d4f51a7b122c10f43a32ef
Corentin <corentin.colart@gmail.com>
2020-07-16 15:34:15 +0200

    Update GameManager.js

--- assets/js/game_manager/GameManager.js
+++ assets/js/game_manager/GameManager.js
@@ -24,16 +24,28 @@ class GameManager {
                 layer.objects.forEach((obj) => {
                     this.playerLocations.push([obj.x, obj.y]);
                 });
-            }
-            else if (layer.name === 'chest_locations') {
+            } else if (layer.name === 'chest_locations') {
                 layer.objects.forEach((obj) => {
-                    this.playerLocations.push([obj.x, obj.y]);
+                    if (this.chestLocations[obj.properties.spawner]) {
+                        this.chestLocations[obj.properties.spawner].push([obj.x, obj.y]);
+                    } else {
+                        this.chestLocations[obj.properties.spawner] = [
+                            [obj.x, obj.y]
+                        ];
+                    }
+                });
+            } else if (layer.name === 'monster_locations') {
+                layer.objects.forEach((obj) => {
+                    if (this.monsterLocations[obj.properties.spawner]) {
+                        this.monsterLocations[obj.properties.spawner].push([obj.x, obj.y]);
+                    } else {
+                        this.monsterLocations[obj.properties.spawner] = [
+                            [obj.x, obj.y]
+                        ];
+                    }
                 });
             }
-            else if (layer.name === 'monster_locations') {
-
-            }
-        })
+        });
     }
 
     setupEventListeners() {
