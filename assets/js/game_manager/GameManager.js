79c7c46f85efe48bae65d10282dc48b839207e2c
Corentin <corentin.colart@gmail.com>
2020-07-16 14:10:17 +0200

    Classes setup

--- assets/js/game_manager/GameManager.js
+++ assets/js/game_manager/GameManager.js
@@ -1,3 +1,13 @@
-class ChestModel {
-    constructor() {}
-  }
+class GameManager {
+    constructor(scene, mapData) {
+        this.scene = scene;
+        this.mapData = mapData;
+
+        this.spawners = {};
+        this.chests = {};
+        
+        this.playerLocations = [];
+        this.chestLocations = {};
+        this.monsterLocations = {};
+    }
+}
