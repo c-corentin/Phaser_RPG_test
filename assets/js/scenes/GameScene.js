20b70f33c1b1cc9e42be15d02e9f9a9786624ef6
Corentin <corentin.colart@gmail.com>
2020-07-16 12:08:29 +0200

    Update GameScene.js
    Update player spawn point

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -26,7 +26,7 @@ class GameScene extends Phaser.Scene {
   }
 
   createPlayer() {
-    this.player = new Player(this, 32, 32, 'characters', 0);
+    this.player = new Player(this, 244, 244, 'characters', 0);
   }
 
   createChests() {
