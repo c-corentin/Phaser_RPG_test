eda7c00bacf480d7a2b6c568765c4bb69f40ba35
Corentin <corentin.colart@gmail.com>
2020-08-07 16:04:26 +0200

    Update GameScene.js
    spawnMonster method updated

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -35,28 +35,65 @@ class GameScene extends Phaser.Scene {
   createGroups() {
     // create a chest group
     this.chests = this.physics.add.group();
+
+    // monster group
+    this.monsters = this.physics.add.group();
   }
 
   spawnChest(chestObject) {
     let chest = this.chests.getFirstDead();
+
     if (!chest) {
-      chest = new Chest(this, chestObject.x * 2, chestObject.y * 2, 'items', 0, chestObject.gold, chestObject.id);
+      chest = new Chest(
+        this,
+        chestObject.x * 2,
+        chestObject.y * 2,
+        'items',
+        0,
+        chestObject.gold,
+        chestObject.id,
+        );
       // add chest to chests group
       this.chests.add(chest);
     } else {
       chest.coins = chestObject.gold; // pass the amount of gold
       chest.id = chestObject.id; // pass the chest id
-      chest.setPosition(chestObject.x * 2, chestObject.y * 2);
+      chest.setPosition(chestObject.x * 2,
+        chestObject.y * 2);
       chest.makeActive();
     }
   }
 
-  spawnMonster(monster) {
-    // WIP
+  spawnMonster(monsterObject) {
+    let monster = this.monsters.getFirstDead();
+
+    if (!monster) {
+      monster = new Monster(
+        this,
+        monsterObject.x * 2,
+        monsterObject.y * 2,
+        'monsters',
+        monsterObject.frame,
+        monsterObject.id,
+        monsterObject.health,
+        monsterObject.maxHealth,
+        );
+      // add monster to monsters group
+      this.monsters.add(monster);
+    } else {
+      monster.id = monsterObject.id; // pass the monster id
+      monster.health = monsterObject.health;
+      monster.maxHealth = monsterObject.maxHealth;
+      monster.setTexture('monsters', monsterObject.frame); // 2 arguments: asset & frame
+      monster.setPosition(monsterObject.x * 2,
+        monsterObject.y * 2);
+      monster.makeActive();
+    }
   }
 
   createInput() {
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this
+      .cursors = this.input.keyboard.createCursorKeys();
   }
 
   addCollisions() {
@@ -75,10 +112,10 @@ class GameScene extends Phaser.Scene {
     this.events.emit('updateScore', this.score);
     // make chest game object inactive
     chest.makeInactive();
-    
+
     this.events.emit('pickUpChest', chest.id);
 
-}
+  }
 
   createMap() {
     // create new map from the Map class
